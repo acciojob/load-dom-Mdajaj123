d5ad2fe3683e35bf1827eef5b26b7ab8b7d0212c
Mdajaj123 <137004218+Mdajaj123@users.noreply.github.com>
2023-10-22 17:58:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
 function input(){
 	const body=document.getElementsByTagName("body")[0];
-	const script=document.getElementsByTagName("script")[0];
+	// const script=document.getElementsByTagName("script")[0];
 	const p=document.createElement("p");
 	p.innerText="DOM load success";
-	body.insertBefore(p,script);
+	body.append(p);
 }
 input();
